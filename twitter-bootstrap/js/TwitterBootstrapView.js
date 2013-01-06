4e89703696fd291e91387afcd5de5d4f5640767a
drq <quyong2000@yahoo.com>
2013-01-06 08:28:49 -0500

    More work on getting correct styles injected.

--- twitter-bootstrap/js/TwitterBootstrapView.js
+++ twitter-bootstrap/js/TwitterBootstrapView.js
@@ -7,19 +7,19 @@
             targetDiv.addClass('control-group');
         },
         "required" : function(targetDiv) {
-            $('<span class="ui-icon ui-icon-star icon-star"></span>').prependTo(targetDiv);
+            $('<span class="icon-star"></span>').prependTo(targetDiv);
         },
         "error" : function(targetDiv) {
-            targetDiv.addClass('ui-state-error control-group error');
+            targetDiv.addClass('control-group error');
         },
         "errorMessage" : function(targetDiv) {
-            targetDiv.addClass('ui-state-error-text');
+            targetDiv.addClass('');
         },
         "removeError" : function(targetDiv) {
             targetDiv.removeClass('error');
         },
         "container" : function(targetDiv) {
-            targetDiv.addClass('ui-widget-content');
+            targetDiv.addClass('');
         },
         "wizardStatusBar" : function(targetDiv) {
             targetDiv.addClass('ui-widget-header ui-corner-all');
@@ -30,35 +30,21 @@
         "wizardUnCurrentStep" : function(targetDiv) {
             targetDiv.removeClass('ui-state-highlight ui-corner-all');
         },
-        "containerExpandedIcon" : "ui-icon-circle-arrow-s",
-        "containerCollapsedIcon" : "ui-icon-circle-arrow-e",
-        "commonIcon" : "ui-icon",
-        "addIcon" : "ui-icon-circle-plus",
-        "removeIcon" : "ui-icon-circle-minus",
-        "upIcon" : "ui-icon-circle-arrow-n",
-        "downIcon" : "ui-icon-circle-arrow-s",
+        "containerExpandedIcon" : "icon-circle-arrow-down",
+        "containerCollapsedIcon" : "icon-circle-arrow-right",
+        "commonIcon" : "",
+        "addIcon" : "icon-plus-sign",
+        "removeIcon" : "icon-minus-sign",
+        "upIcon" : "icon-chevron-up",
+        "downIcon" : "icon-chevron-down",
         "wizardPreIcon" : "ui-icon-triangle-1-w",
         "wizardNextIcon" : "ui-icon-triangle-1-e",
         "wizardDoneIcon" : "ui-icon-triangle-1-e",
         "buttonBeautifier"  : function(button, iconClass, withText) {
-            button.addClass("ui-button ui-widget ui-state-default ui-corner-all");
-            if (withText) {
-                button.addClass("ui-button-text-icon-primary");
-            } else {
-                button.addClass("ui-button-icon-only");
-            }
             var buttonText = button.html();
             button.attr("title", buttonText);
-            button.empty().append('<span class="ui-button-icon-primary ui-icon alpaca-fieldset-legend-button ' + iconClass + '"></span><span class="ui-button-text">' + buttonText + '</span>');
-            button.hover(function() {
-                if (!button.hasClass("alpaca-fieldset-array-item-toolbar-disabled")) {
-                    $(this).addClass("ui-state-hover");
-                }
-            }, function() {
-                if (!button.hasClass("alpaca-fieldset-array-item-toolbar-disabled")) {
-                    $(this).removeClass("ui-state-hover");
-                }
-            });
+            var addedButtonText = withText ? buttonText : "";
+            button.empty().append('<i class="alpaca-fieldset-legend-button ' + iconClass + '"></i> ' + addedButtonText + '</span>');
         }
     };
 
@@ -69,13 +55,28 @@
         "description": "Web edit view for twitter bootstrap.",
         "style":"twitter-bootstrap",
         "templates": {
-            "controlFieldMessage": '<div><span class="icon-warning-sign"></span><span class="alpaca-controlfield-message-text help-inline">${message}</span></div>'
+            "controlFieldLabel": '{{if options.label}}<div class="{{if options.labelClass}}${options.labelClass}{{/if}}"> ${options.label}</div>{{/if}}',
+            "controlFieldHelper": '{{if options.helper}}<div class="{{if options.helperClass}}${options.helperClass}{{/if}}"><i class="icon-info-sign"></i> <span class="alpaca-controlfield-helper-text">${options.helper}</span></div>{{/if}}',
+            "controlFieldMessage": '<div><span class="icon-exclamation-sign"></span><span class="alpaca-controlfield-message-text help-inline">${message}</span></div>',
+            "arrayToolbar":'<span class="alpaca-fieldset-array-toolbar"><button class="btn btn-small alpaca-fieldset-array-toolbar-icon alpaca-fieldset-array-toolbar-add">${addItemLabel}</button></span>',
+            "arrayItemToolbar": '<div class="btn-toolbar alpaca-fieldset-array-item-toolbar"><div class="btn-group"><button class="btn btn-small alpaca-fieldset-array-item-toolbar-icon alpaca-fieldset-array-item-toolbar-add">${addItemLabel}</button><button class="btn btn-small alpaca-fieldset-array-item-toolbar-icon alpaca-fieldset-array-item-toolbar-remove">${removeItemLabel}</button><button class="btn btn-small alpaca-fieldset-array-item-toolbar-icon alpaca-fieldset-array-item-toolbar-up">${moveUpItemLabel}</button><button class="btn btn-small alpaca-fieldset-array-item-toolbar-icon alpaca-fieldset-array-item-toolbar-down">${moveDownItemLabel}</button></div></div>',
+            "controlFieldCheckbox": '<span>{{if options.rightLabel}}<label for="${id}" class="checkbox">{{/if}}<input type="checkbox" id="${id}" {{if options.readonly}}readonly="readonly"{{/if}} {{if name}}name="${name}"{{/if}} {{each(i,v) options.data}}data-${i}="${v}"{{/each}}/>{{if options.rightLabel}}${options.rightLabel}</label>{{/if}}</span>',
+            "controlFieldRadio": '<div id="${id}" class="alpaca-controlfield-radio">{{if !required}}<label class="alpaca-controlfield-radio-label radio inline"><input type="radio" {{if options.readonly}}readonly="readonly"{{/if}} name="${name}" value=""/>None</label>{{/if}}{{each selectOptions}}<label class="alpaca-controlfield-radio-label radio inline"><input type="radio" {{if options.readonly}}readonly="readonly"{{/if}} name="${name}" value="${value}" {{if value == data}}checked="checked"{{/if}}/>${text}</label>{{/each}}</div>'
         },
         "render": function(field, renderedCallback) {
 
             field.render(function(field) {
 
                 $('select,input[type=text], textarea', field.outerEl).addClass('input-xlarge');
+                $('input:submit, input:reset, .alpaca-form-button').addClass('btn').removeClass('alpaca-form-button');
+
+                // Inject addon icon to phone/email field
+                /*
+                $('.alpaca-controlfield-phone', field.outerEl).addClass('input-prepend').prepend('<span class="add-on">&#9990;</span>')
+                $('.alpaca-controlfield-email', field.outerEl).addClass('input-prepend').prepend('<span class="add-on"><i class="icon-envelope"></i></span>')
+                $('.alpaca-controlfield-time', field.outerEl).addClass('input-prepend').prepend('<span class="add-on"><i class="icon-time"></i></span>')
+                $('.alpaca-controlfield-date', field.outerEl).addClass('input-prepend').prepend('<span class="add-on"><i class="icon-calendar"></i></span>')
+                */
 
                 if (renderedCallback) {
                     renderedCallback(field);
